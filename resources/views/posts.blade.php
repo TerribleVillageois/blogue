20b450b4e6074c3dd4772a100216fca04c6403c6
TerribleVillageois <maximeboivin31416@gmail.com>
2022-06-03 09:15:04 -0400

    Test4

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+
+
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My first post </a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium neque totam veritatis vel. Est cupiditate possimus, soluta dicta velit minus at amet tenetur culpa! Voluptatum, quae. Consequatur, quo reprehenderit.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">My second post </a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium neque totam veritatis vel. Est cupiditate possimus, soluta dicta velit minus at amet tenetur culpa! Voluptatum, quae. Consequatur, quo reprehenderit.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-third-post">My third post </a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium neque totam veritatis vel. Est cupiditate possimus, soluta dicta velit minus at amet tenetur culpa! Voluptatum, quae. Consequatur, quo reprehenderit.
+    </p>
+</article>
+
+<article>
+    <h1> <a href="/posts/my-fourth-post">my last post</a> </h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium neque totam veritatis vel. Est cupiditate possimus, soluta dicta velit minus at amet tenetur culpa! Voluptatum, quae. Consequatur, quo reprehenderit.
+    </p>
+</article>
+
+</body>
